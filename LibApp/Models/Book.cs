86d19e6264777139291231151dab2af6138515e6
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-17 16:53:33 +0100

    task1 Adding ErrorMessage to the book class

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -5,16 +5,20 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter the title of the book")]
         [StringLength(100)]
         public string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter the Author of the book")]
         public string Author { get; set; }
         [Required]
         public Genre Genre { get; set; }
+        [Required(ErrorMessage = "Please enter the Genre of the book")]
         public int GenreId { get; set; }
         public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "Please select the book's release date")]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "How many books are there?")]
+        [Range(1, 20, ErrorMessage = "Range from 1 to 20")]
         public byte NumberInStock { get; set; }
     }
 }
